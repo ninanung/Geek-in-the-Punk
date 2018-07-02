44fe905dcbcb7b777f7f2e247298c23e73d3d62d
Seungje Jun <ninanung@naver.com>
2018-07-02 10:08:17 +0900

    daily last

--- blog/src/components/navigation/router-links/YearLinks.vue
+++ blog/src/components/navigation/router-links/YearLinks.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="year-links">
-        <p>{{this.year}}</p>
-        <link-list v-bind:key="index" v-for="(toWhere, index) in yearLink" :year="year" :toWhere="toWhere" :index="index" />
+        <p v-on:click="this.yearListPopUp">{{this.year}}</p>
+        <link-list v-if="isView" v-bind:key="index" v-for="(toWhere, index) in yearLink" :year="year" :toWhere="toWhere" :index="index" />
     </div>
 </template>
 <script>
@@ -10,9 +10,22 @@ import LinkList from './LinkList.vue'
 
 export default {
     components: { LinkList },
-    props: [ 'year', 'yearLink' ]
+    props: [ 'year', 'yearLink' ],
+    data: function() {
+        return {
+            isView: false
+        }
+    },
+    methods: {
+        yearListPopUp: function() {
+            this.isView = !this.isView
+        }
+    }
 }
 </script>
 <style scoped>
-
+    p {
+        width: 4%; margin: 5px 48% 5px 48%;
+        cursor: pointer;
+    }
 </style>
